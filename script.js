7fe25d254844b8630ae8d82f915e02a80057457a
Nithin.S <nithin.s491@gmail.com>
2020-03-15 13:45:54 +0530

    Added PWA

--- script.js
+++ script.js
@@ -39,4 +39,41 @@ async function registerSW() {
       }
     }
 }
-  
+
+var deferredPrompt;window.addEventListener('beforeinstallprompt', function (e) { 
+	e.preventDefault(); 
+	deferredPrompt = e;
+	showAddToHomeScreen();
+});
+
+function showAddToHomeScreen() {
+	var a2hsBtn = document.querySelector(".ad2hs-prompt");
+	a2hsBtn.style.display = "block";
+	a2hsBtn.addEventListener("click", addToHomeScreen);
+}
+
+function addToHomeScreen() {
+ 	var a2hsBtn = document.querySelector(".ad2hs-prompt");
+	a2hsBtn.style.display = 'none';
+	deferredPrompt.prompt();
+	deferredPrompt.userChoice .then(function(choiceResult){
+	if (choiceResult.outcome === 'accepted') {
+    	console.log('User accepted the A2HS prompt');
+	} else {
+    	console.log('User dismissed the A2HS prompt'); 
+	}
+	deferredPrompt = null;
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
