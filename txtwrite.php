420b62ebe6cfcf94db68fad1f23ebb9cde7385b4
Nithin.S <nithin.s491@gmail.com>
2020-03-18 12:00:18 +0530

    Update txtwrite.php

--- txtwrite.php
+++ txtwrite.php
@@ -3,6 +3,6 @@ if(isset($_POST['btn'])){
     $num=$_POST['number'];
     $txt=$_POST['txt'];
     exec("echo \".$txt.\">TEXT/".$num.".txt");
-    echo '<script>alert("Text write successful")</script>';
+    echo '<script>var r = confirm("Text write successful");window.location.href ="https://tempcloud.ml";</script>';
   }
 ?>
